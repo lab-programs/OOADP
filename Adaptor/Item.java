fa57bdd88b930cb0295e6457f96505707bcc0c95
tezz-io <matblockgreninja@gmail.com>
2021-07-08 09:08:55 -0700

    Added the instance variable name and the constructor to initialize an item.

--- Adaptor/Item.java
+++ Adaptor/Item.java
@@ -1,4 +1,11 @@
 public class Item {
+    private String name;
     private int quantity;
     private double price;
+
+    public Item( String name, int quantity, double price ) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
 }
