eddf08b65bf4bf0a0a9651cf4d24f7b760a4152c
tezz-io <matblockgreninja@gmail.com>
2021-07-08 09:46:52 -0700

    Added the Strategy folder with the Client.java file containing the problem statement commented out.

---
+++ Strategy/Client.java
@@ -0,0 +1,19 @@
+/*
+
+You are a fresh analyst deputed to design the software for Decathlon Chain of
+Stores in Karnataka. You are informed about the Business Logic of Point of
+Sales criteria by Ms.Veronica Lodge, a dynamic business tycoon operating out of
+Decathlon Mumbai. She informs you that there are different types of Customers
+of Decathlon namely, Regular Customers, Senior Citizens and First Time
+Customers. Regular Customers are given a discount of 12%, Senior Citizens 10%
+and First Time Customers 15%. Apart from this, based on the sales-index of
+previous day, a Store-level discount is determined every day. This is dynamic.
+E.g.Rs.100 off for every purchase above Rs.2000. Using the Object Oriented
+Principles of Encapsulation, Abstraction, Inheritance, Composition and
+Aggregation that you have studied until this semester, give at least two ways
+to design this system.
+
+How will you use the Strategy Pattern to tackle the limitations of traditional
+Object Oriented Design highlighted in PART A? The design must handle varying
+price-schemes having different pricing algorithms. Design & implement.
+*/
