906cd67dfd288b76c8a6e347490dfdaf3a64dcc6
tezz-io <matblockgreninja@gmail.com>
2021-07-08 09:51:55 -0700

    Added RegularCustomer class which is a customer with discount = 12.0

---
+++ Strategy/RegularCustomer.java
@@ -0,0 +1,6 @@
+public class RegularCustomer extends Customer {
+    private double discount = 12.0;
+
+    public double getDiscount() { return this.discount; }
+    public void setDiscount( double discount ) { this.discount = discount; }
+}
