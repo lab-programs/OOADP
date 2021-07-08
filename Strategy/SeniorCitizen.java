54b2e07e2e690e33552596107e038d14f51115cd
tezz-io <matblockgreninja@gmail.com>
2021-07-08 09:54:59 -0700

    Added class SeniorCitizen which is a customer with discount 10%.

---
+++ Strategy/SeniorCitizen.java
@@ -0,0 +1,6 @@
+public class SeniorCitizen extends Customer {
+    private double discount = 10.0;
+
+    public double getDiscount() { return this.discount; }
+    public void setDiscount( double discount ) { this.discount = discount; }
+}
