264daa79db454fb6e59d512ccb361ef845f62859
tezz-io <matblockgreninja@gmail.com>
2021-07-08 10:25:33 -0700

    Changed StoreLevelDiscount class to have minimumPurchase and discountAmount properties, respective getters and setters and applyDiscount funciton which appiles discount only if the price is greater than minimum purchase.

--- Strategy/StoreLevelDiscount.java
+++ Strategy/StoreLevelDiscount.java
@@ -1,6 +1,17 @@
 public class StoreLevelDiscount {
-    private double discount;
+    private double minimumPurchase = 2000.0;
+    private double discountAmount = 100.0;
 
-    public double getDiscount() { return this.discount; }
-    public void setDiscount( double discount ) { this.discount = discount; }
+    public double getMinimumPurchase() { return this.minimumPurchase; }
+    public void setMinimumPurchase( double minimumPurchase ) { this.minimumPurchase = minimumPurchase; }
+
+    public double getDiscountAmount() { return this.discountAmount; }
+    public void setDiscountAmount( double discountAmount ) { this.discountAmount = discountAmount; }
+
+    public double applyDiscount( double price ) {
+        if( price > minimumPurchase ) {
+            return price - discountAmount;
+        }
+        return price;
+    }
 }
