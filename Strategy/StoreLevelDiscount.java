2848919040b4aea9750de38ed139a161a4b7cbac
tezz-io <matblockgreninja@gmail.com>
2021-07-08 10:12:38 -0700

    Added StoreLevelDiscount.

---
+++ Strategy/StoreLevelDiscount.java
@@ -0,0 +1,6 @@
+public class StoreLevelDiscount extends Customer {
+    private double discount;
+
+    public double getDiscount() { return this.discount; }
+    public void setDiscount( double discount ) { this.discount = discount; }
+}
