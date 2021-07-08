c210c2c53f8a2d8a2d9b1d7099d774da87b97218
tezz-io <matblockgreninja@gmail.com>
2021-07-08 09:57:09 -0700

    Added FirstTimeCustomer class which is a customer class with 15% discount.

---
+++ Strategy/FirstTimeCustomer.java
@@ -0,0 +1,6 @@
+public class FirstTimeCustomer extends Customer {
+    private double discount = 15.0;
+
+    public double getDiscount() { return this.discount; }
+    public void setDiscount( double discount ) { this.discount = discount; }
+}
