f3271060a31f88b403145fa3a2ed4d03fc3e3c9c
tezz-io <matblockgreninja@gmail.com>
2021-07-08 08:37:39 -0700

    Added Client, MauriTax and TaxCalculator classes and interfaces

---
+++ Adaptor/Client.java
@@ -0,0 +1,17 @@
+/*
+To establish the 1st Decathlon store in Mauritius, you go along with Mr. Satya Nadella,
+an expert in finding 3rd-party partners. For e.g. a 3rd-party Tax- Calculator system
+to cater to the specifics of Sales and VAT (Value-added services Tax) tax calculations
+in different countries. He finds a 3rd-party Tax-Calculator system called ‘MauriTax’
+in Port Louis. The problem is, the APIs used by ‘MauriTax’ for tax-calculation is
+fixed & cannot be changed. The ‘MauriTax’ APIs are incompatible with ‘Decathlon POS’.
+
+How will you use the Adaptor Pattern to design & implement?
+*/
+
+public class Client {
+    public static void main( String[] args ) {
+        String s = "This is a string.";
+        System.out.println(s);
+    }
+}
---
+++ Adaptor/MauriTax.java
@@ -0,0 +1,5 @@
+public class MauriTax {
+    public double mauriTax( double price, int quantity ) {
+        return 0.14 * price * (double) quantity;
+    }
+}
