58e99996e0a3ebdeb54ff4586de18a4b85f13dbe
tezz-io <matblockgreninja@gmail.com>
2021-07-09 02:51:32 -0700

    Added the new compilations to be done - StoreLevelDiscount, DayOneStoreLevelDiscount and DayTwoStoreLevelDiscount classes to the compile.sh script.

--- Strategy/compile.sh
+++ Strategy/compile.sh
@@ -2,4 +2,7 @@ javac Customer.java
 javac RegularCustomer.java
 javac SeniorCitizen.java
 javac FirstTimeCustomer.java
+javac StoreLevelDiscount.java
+javac DayOneStoreLevelDiscount.java
+javac DayTwoStoreLevelDiscount.java
 javac Client.java
