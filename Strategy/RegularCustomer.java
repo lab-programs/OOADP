1de976ee87e45f3ff150ae0c6f836122aae1c1e0
tezz-io <matblockgreninja@gmail.com>
2021-07-09 02:31:01 -0700

    Added discount to static block and made changes to getters and setters in RegularCustomer class.

--- Strategy/RegularCustomer.java
+++ Strategy/RegularCustomer.java
@@ -1,11 +1,13 @@
 public class RegularCustomer extends Customer {
+    static {
+        RegularCustomer.discount = 12.0;
+    }
 
-    public double getDiscount() { return this.discount; }
-    public void setDiscount( double discount ) { this.discount = discount; }
+    public double getDiscount() { return RegularCustomer.discount; }
+    public void setDiscount( double discount ) { RegularCustomer.discount = discount; }
 
     public RegularCustomer( String name, int age ) {
         this.name = name;
         this.age = age;
-        this.discount = 12.0;
     }
 }
