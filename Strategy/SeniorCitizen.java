0f0ff45cc9e5eea94df371f3aedf15684141beea
tezz-io <matblockgreninja@gmail.com>
2021-07-09 02:32:00 -0700

    Added discount to static block and made changes to getters and setters in SeniorCitizen class.

--- Strategy/SeniorCitizen.java
+++ Strategy/SeniorCitizen.java
@@ -1,11 +1,12 @@
 public class SeniorCitizen extends Customer {
-
-    public double getDiscount() { return this.discount; }
-    public void setDiscount( double discount ) { this.discount = discount; }
+    static {
+        SeniorCitizen.discount = 10.0;
+    }
+    public double getDiscount() { return SeniorCitizen.discount; }
+    public void setDiscount( double discount ) { SeniorCitizen.discount = discount; }
 
     public SeniorCitizen( String name, int age ) {
         this.name = name;
         this.age = age;
-        this.discount = 10;
     }
 }
