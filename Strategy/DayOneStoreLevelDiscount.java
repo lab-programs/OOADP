f0cf9aecda95e2a55a715f57a477164703956726
tezz-io <matblockgreninja@gmail.com>
2021-07-09 02:48:35 -0700

    Added two days store level discount.

---
+++ Strategy/DayOneStoreLevelDiscount.java
@@ -0,0 +1,6 @@
+public class DayOneStoreLevelDiscount extends StoreLevelDiscount {
+    public DayOneStoreLevelDiscount() {
+        this.minimumPurchase = 2000.0;
+        this.discountAmount = 100.0;
+    }
+}
---
+++ Strategy/DayTwoStoreLevelDiscount.java
@@ -0,0 +1,6 @@
+public class DayTwoStoreLevelDiscount extends StoreLevelDiscount {
+    public DayTwoStoreLevelDiscount() {
+        this.minimumPurchase = 2500.0;
+        this.discountAmount = 200.0;
+    }
+}
