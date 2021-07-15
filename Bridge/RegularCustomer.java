f1a73942228ac6d0d67fb088ba1d6be537cbb014
tezz-io <matblockgreninja@gmail.com>
2021-07-15 11:06:39 -0700

    [Process] Added a derivation of the Customer class called RegualarCustomer.

---
+++ Bridge/RegularCustomer.java
@@ -0,0 +1,7 @@
+public class RegularCustomer extends Customer {
+    public RegularCustomer( String name, int age, DisplayCalculator displayCalculator ) {
+        this.name = name;
+        this.age = age;
+        this.displayCalculator = displayCalculator;
+    }
+}
