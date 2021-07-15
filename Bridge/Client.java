7d4eeb8792d14a0480b1ce213f7aedfbad6f7b82
tezz-io <matblockgreninja@gmail.com>
2021-07-15 09:57:21 -0700

    Added the problem statement and an empty stub main method in class Client.

---
+++ Bridge/Client.java
@@ -0,0 +1,21 @@
+/*
+Bridge (Structural): You get a call from Ms.Masaba Gupta of Bangalore Decathlon
+office that there is a policy decision made globally to introduce discount
+slabs for a whole month twice in a year. The discount month will be in January 
+and July after reviewing the sales made from Feb to June (first five months)
+and Aug to December (last five months) respectively. It is decided to provide
+four slabs of discounts in 2017, namely, 30%, 25%, 20% and 15%, based on the
+sports item purchased. For e.g. all tennis rackets could have a 20% discount
+while cricket bats could only have a 15% discount. All exercise tread-mills
+could be given a 30% discount while boxing-gloves could have a 25% discount.
+Point to be noted here is that, the slabs of discount may not remain the same
+in 2018. It is likely to vary year after year. The ‘Decathlon POS’ software
+system classifies its customers as Senior-Citizens (60 and above), First-Time
+Customers, Regular Customers as of now. There is a very high possibility that
+the Customer Type hierarchy will vary, depending upon the sales-pattern. For
+e.g. there could be the need to introduce new categories based on the customer
+gender.
+Use the Bridge Pattern to design & implement, so that both the Customer Type
+hierarchy of classes as well as the Discount Percentage hierarchy of classes
+can both vary independently? That is, they are not tied to each other.
+*/
