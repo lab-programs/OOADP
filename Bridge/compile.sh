de7272a0d8c4bcecc57d45cc13ec7b697a2360e8
tezz-io <matblockgreninja@gmail.com>
2021-07-16 04:30:35 -0700

    [Process] Added compile.sh to automate compilation process.

---
+++ Bridge/compile.sh
@@ -0,0 +1,8 @@
+javac DiscountCalculator.java
+javac FirstDC.java
+javac SecondDC.java
+javac Customer.java
+javac RegularCustomer.java
+javac SeniorCitizen.java
+javac FirstTimeCustomer.java
+javac Client.java
