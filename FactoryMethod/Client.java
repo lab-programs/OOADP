4b08145f33f6b3725a3bfaedd974cf41302b870d
tezz-io <matblockgreninja@gmail.com>
2021-08-07 19:41:49 +0530

    Removed private static class A

--- FactoryMethod/Client.java
+++ FactoryMethod/Client.java
@@ -1,12 +1,6 @@
 public class Client {
-    private static class A extends Customer {
-        A( String name, int age ) {
-            super( name, age );
-            super.category = AgeCategory.kid;
-        }
-    }
     public static void main( String[] args ) {
-        A a = new A("A", 1);
-        a.details();
+        Kid k = new Kid("A", 1);
+        k.details();
     }
 }
