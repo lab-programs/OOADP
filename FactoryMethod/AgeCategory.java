b994df844c3f05d3a376ed58f3311f8b5e970077
tezz-io <matblockgreninja@gmail.com>
2021-08-07 18:28:04 +0530

    Added String to enum AgeCategory

--- FactoryMethod/AgeCategory.java
+++ FactoryMethod/AgeCategory.java
@@ -2,5 +2,9 @@ public enum AgeCategory {
     kid("Kid"),
     teen("Teen"),
     adult("Adult"),
-    senior("Senior")
+    senior("Senior");
+
+    private final String text;
+    AgeCategory( final String text ) { this.text = text; }
+    @Override public String toString() { return text; }
 }
