702e6f0f23019331f1ecc1c1f81ca42b2d6de443
tezz-io <matblockgreninja@gmail.com>
2021-08-07 19:53:57 +0530

    Generated build files

--- FactoryMethod/Client.java
+++ FactoryMethod/Client.java
@@ -1,6 +1,14 @@
 public class Client {
     public static void main( String[] args ) {
-        Kid k = new Kid("A", 1);
-        k.details();
+        TestCustomer( new Kid("A", 1) );
+        TestCustomer( new Teen("B", 13) );
+        TestCustomer( new Adult("C", 18) );
+        TestCustomer( new Senior("D", 61) );
+    }
+
+    public static void TestCustomer( Customer customer ) {
+        System.out.println("-------------------------");
+        customer.details();
+        System.out.println("-------------------------");
     }
 }
